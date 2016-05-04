b31da2c4d3be75452a61d77c3830aa425b95f965
ergobot <stobryan@gmail.com>
2016-05-03 19:16:01 -0500

    added files and onclick

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button sound8;
     Button sound9;
 
+    MediaPlayer mediaPlayer = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // call the parent
@@ -30,26 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
         // associate your declared button object with
         // the button in activity_main.xml
-        sound1 = (Button)findViewById(R.id.sound1);
-        sound2 = (Button)findViewById(R.id.sound2);
-        sound3 = (Button)findViewById(R.id.sound3);
-        sound4 = (Button)findViewById(R.id.sound4);
-        sound5 = (Button)findViewById(R.id.sound5);
-        sound6 = (Button)findViewById(R.id.sound6);
-        sound7 = (Button)findViewById(R.id.sound7);
-        sound8 = (Button)findViewById(R.id.sound8);
-        sound9 = (Button)findViewById(R.id.sound9);
+        sound1 = (Button)findViewById(R.id.chopin);
+        sound2 = (Button)findViewById(R.id.beethoven);
+        sound3 = (Button)findViewById(R.id.dvorak);
+        sound4 = (Button)findViewById(R.id.brahms);
+        sound5 = (Button)findViewById(R.id.schumann);
+        sound6 = (Button)findViewById(R.id.chopin2);
+        sound7 = (Button)findViewById(R.id.haydn);
+        sound8 = (Button)findViewById(R.id.beethoven2);
+        sound9 = (Button)findViewById(R.id.brahms2);
+
+
 
         // do something when you click on the buttons
         sound1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // OPTIONAL: show a message when playing a sound
-                // showToast("Playing a sound");
+
 
                 // Play a sound
-//                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sound_file_1);
-//                mediaPlayer.start();
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else {
+                    showToast("Waltz in C# minor, Op. 64, No. 2");
+                    // ref:  https://en.wikipedia.org/wiki/Fr%C3%A9d%C3%A9ric_Chopin
+                    // ref:  https://en.wikipedia.org/wiki/Waltz_in_C-sharp_minor,_Op._64,_No._2_(Chopin)
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.chopinwaltzop64no2incsharpminor);
+                    mediaPlayer.start();
+                }
+
             }
         });
 
@@ -57,12 +70,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Symphony No. 7, Op. 92: II. Allegretto");
+
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Ludwig_van_Beethoven
+                    // ref:  https://en.wikipedia.org/wiki/File:JOHN_MICHEL_CELLO-BEETHOVEN_SYMPHONY_7_Allegretto.ogg
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.johnmichelcellobeethovensymphony7allegretto);
+                    mediaPlayer.start();
+                }
             }
         });
 
         sound3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Symphony No. 9, Op. 95: IV. Allegro Con Fuoco");
+
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Symphony_No._9_(Dvo%C5%99%C3%A1k)
+                    // ref:  https://en.wikipedia.org/wiki/File:Antonin_Dvorak_-_symphony_no._9_in_e_minor_%27from_the_new_world%27,_op._95_-_iv._allegro_con_fuoco.ogg
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.dvoraksymphonyno9op95iv);
+                    mediaPlayer.start();
+                }
 
             }
         });
@@ -71,40 +111,116 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Symphony No. 3, Op. 90 - III Poco allegretto");
+
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Johannes_Brahms
+                    // ref:  https://en.wikipedia.org/wiki/File:Brahms,_Symphony_No._3_in_F_Major,_Op._90_-_III._Poco_allegretto.ogg
+                    // ref:  https://en.wikipedia.org/wiki/File:Johannes_Brahms_-_Ungarischer_Tanz_5_g-moll.ogg
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.brahmssymphony);
+                    mediaPlayer.start();
+                }
+
             }
         });
 
         sound5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Kinderszenen, Op. 15: Traumerei");
 
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Robert_Schumann
+                    // ref:  https://en.wikipedia.org/wiki/Kinderszenen
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.schumannkinderszenenop15vii);
+                    mediaPlayer.start();
+                }
             }
         });
 
         sound6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Play a sound
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else {
+                    showToast("Etudes, Op 10, No. 3 - Tristesse");
+                    // ref:  https://en.wikipedia.org/wiki/Fr%C3%A9d%C3%A9ric_Chopin
+                    // ref:  https://en.wikipedia.org/wiki/File:Frederic_Chopin_-_Opus_10_-_Twelve_Grand_Etudes_-_E_Major.ogg
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.chopinopus10tristesse);
+                    mediaPlayer.start();
+                }
             }
         });
 
         sound7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Sonata in F major, Hob.XVI:23");
 
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Joseph_Haydn
+                    // ref:  https://upload.wikimedia.org/wikipedia/commons/1/1c/Haydn_sonata_hob._XVI_23_mvt_2.oga
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.haydnsonata);
+                    mediaPlayer.start();
+                }
             }
         });
 
         sound8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Sonata No. 8, Op. 13 - I. Grave");
 
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Ludwig_van_Beethoven
+                    // ref:  https://en.wikipedia.org/wiki/File:Beethoven,_Sonata_No._8_in_C_Minor_Pathetique,_Op._13_-_I._Grave_-_Allegro_di_molto_e_con_brio.ogg                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.johnmichelcellobeethovensymphony7allegretto);
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.beethovensonatana);
+                    mediaPlayer.start();
+                }
             }
         });
 
         sound9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    showToast("Stopped sound");
+                }else{
+                    // OPTIONAL: show a message when playing a sound
+                    showToast("Rhapsody in G minor, Op. 79, No. 2");
+
+                    // Play a sound
+                    // ref:  https://en.wikipedia.org/wiki/Johannes_Brahms
+                    // ref:  https://en.wikipedia.org/wiki/Rhapsodies,_Op._79_(Brahms)
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.brahms_op79);
+                    mediaPlayer.seekTo(585000);
+                    mediaPlayer.start();
+                }
 
             }
         });
